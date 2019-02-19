c1b5a6412980b4b9fda43b25fc63af29fb7e05ea
Alicia Santiago <albrown07@mac.com>
2019-02-19 17:35:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -16,3 +17,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 end
+binding.pry
