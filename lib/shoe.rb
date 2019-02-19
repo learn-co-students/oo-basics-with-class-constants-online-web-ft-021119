d17f67c6c1b5658f93010c09bdd3043347d4bbc7
Alicia Santiago <albrown07@mac.com>
2019-02-19 17:18:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -11,7 +10,7 @@ class Shoe
         BRANDS << @brand
       end
   end
-binding.pry
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
