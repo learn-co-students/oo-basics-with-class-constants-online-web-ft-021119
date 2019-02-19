b45891f1c0852cf753708e49bf2b03dd17f6a41f
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-19 23:02:35 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,20 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+
+    arrayBrand = brand.split
+    arrayBrand = arrayBrand.uniq
+    
+      
+    end
+    
+    uniqueBrand = brand.uniq
+    BRANDS << uniqueBrand
   end
 
   def cobble
